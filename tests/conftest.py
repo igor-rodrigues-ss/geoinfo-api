725f665a11fb7ea00f556045d8cec104c837e67c
Igor Rodrigues <igorrss652@gmail.com>
2022-07-29 22:36:00 -0300

    Fix position of code that start database in tests for the DATABASE_URL will be loaded with correct value.

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,18 +3,17 @@ import os
 import testing.postgresql
 
 from scripts.apply import apply
-from src.config import ROOT_DIR
-
-
-pytest_plugins = ["tests.plugins.base_fixtures"]
 
 print("Starting database...")
 
 postgresql = testing.postgresql.Postgresql()
-
 os.environ["DATABASE_URL"] = postgresql.url()
 
 print("Successful.")
+
+from src.config import ROOT_DIR
+
+pytest_plugins = ["tests.plugins.base_fixtures"]
 
 
 def get_file_from_fixtures(fname: str) -> str:
